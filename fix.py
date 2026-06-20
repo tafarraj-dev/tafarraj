cae45ec7bdcfe40cd4fcacd0bc1c37c856e40112
Aicha <aichakelia01@gmail.com>
2026-06-20 19:43:32 +0100

    Apply gitignore

--- fix.py
+++
@@ -1,9 +0,0 @@
-import os
-import django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dramahere.settings')
-django.setup()
-from django.db import connection
-cursor = connection.cursor()
-cursor.execute("DELETE FROM django_migrations WHERE app = 'Tafarraj'")
-connection.connection.commit()
-print('done')
